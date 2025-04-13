11c8188df4ecd6857d649004df2f02bc604a2914
chocho030 <chocho0003@naver.com>
2025-04-13 15:44:08 +0900

    index js 애니메이션, 링크 함수 개션

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const ticket_back = document.getElementById("ticket-back");
-const ticket_content = document.getElementById("ticket-content");
-const ticket_button = document.getElementById("ticket-button");
+const ticket_back = [
+  document.getElementById("impress-back"),
+  document.getElementById("mucha-back"),
+  document.getElementById("gogh-back"),
+];
+const ticket_content = [
+  document.getElementById("impress-content"),
+  document.getElementById("mucha-content"),
+  document.getElementById("gogh-content"),
+];
+const ticket_button = [
+  document.getElementById("impress-button"),
+  document.getElementById("mucha-button"),
+  document.getElementById("gogh-button"),
+];
 let 가로 = 0;
 let 세로 = 0;
 
-function ani() {
-  ticket_back.style.transform = "rotate(25deg)";
+function ani(num) {
+  const back = ticket_back[num];
+  const content = ticket_content[num];
+  const button = ticket_button[num];
+
+  back.style.transform = "rotate(25deg)";
 
   // 가로, 세로 이동
   가로 = 가로 + 70;
   세로 = 세로 + 45;
-  ticket_back.style.marginLeft = 가로;
-  ticket_back.style.marginTop = 세로;
+  back.style.marginLeft = 가로;
+  back.style.marginTop = 세로;
 
-  ticket_back.style.backgroundColor = "rgba(128, 128, 128, 0)";
-  ticket_content.style.display = "none";
-  ticket_button.style.display = "none";
+  back.style.backgroundColor = "rgba(128, 128, 128, 0)";
+  content.style.display = "none";
+  button.style.display = "none";
 }
 
-function link() {
+const link_list = [
+  "../impress-html/impress.html",
+  "../mucha-html/mucha.html",
+  "../gogh-html/gogh.html",
+];
+function link(num) {
   setTimeout(() => {
-    window.location.href = "../gogh-html/gogh.html";
+    window.location.href = link_list[num];
   }, 2000);
 }
