7180a2548d3a13602fdfd26146b6cbc2e91440ec
chocho030 <chocho0003@naver.com>
2025-04-15 22:49:02 +0900

    gogh-review 페이지네이션 개선

--- gogh-js/gogh-review.js
+++ gogh-js/gogh-review.js
@@ -1,25 +1,35 @@
-const content_list = [
-  document.getElementById("one"),
-  document.getElementById("two"),
-  document.getElementById("three"),
-  document.getElementById("four"),
-];
+// const content_list = [
+//   document.getElementById("one"),
+//   document.getElementById("two"),
+//   document.getElementById("three"),
+//   document.getElementById("four"),
+// ];
+
+let content_list = document.getElementsByClassName("main-content");
 let list_length = content_list.length;
 
-let content_main = document.getElementById("one");
+let content_main = content_list[0];
 content_main.style.display = "flex";
 let num = 1;
+console.log(content_list);
+console.log("list_length : ", list_length);
 
+console.log("review");
 // 이전
 function before_btn(page_num) {
   content_main.style.display = "none";
   num--;
+  console.log("num : ", num);
 
   if (num == 0) {
     page(page_num);
   } else {
     content_main = content_list[num - 1];
     content_main.style.display = "flex";
+  }
+
+  if (page_num == 0 && num == 1) {
+    document.getElementById("left").style.display = "none";
   }
 }
 
@@ -29,6 +39,7 @@ function next_btn(page_num) {
 
   content_main.style.display = "none";
   num++;
+  console.log("num : ", num);
 
   if (num == list_length + 1) {
     page(page_num);
