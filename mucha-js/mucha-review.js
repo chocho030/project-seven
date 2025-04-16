58373eb82a2821222d9af62c2ec1ecb66dc5f2d2
chocho030 <chocho0003@naver.com>
2025-04-17 00:27:49 +0900

    impress, mucha 변경된 양식으로 수정

--- mucha-js/mucha-review.js
+++ mucha-js/mucha-review.js
@@ -1,38 +1,61 @@
-const content_list = [
-  document.getElementById("one"),
-  document.getElementById("two"),
-  document.getElementById("three"),
-  document.getElementById("four"),
-];
+// const content_list = [
+//   document.getElementById("one"),
+//   document.getElementById("two"),
+//   document.getElementById("three"),
+//   document.getElementById("four"),
+// ];
+
+let content_list = document.getElementsByClassName("main-content");
 let list_length = content_list.length;
 
-let content_main = document.getElementById("one");
+let content_main = content_list[0];
 content_main.style.display = "flex";
-let num = 0;
+let num = 1;
+console.log(content_list);
+console.log("list_length : ", list_length);
 
-function before_btn() {
+console.log("review");
+// 이전
+function before_btn(page_num) {
   content_main.style.display = "none";
   num--;
-  content_main = content_list[num];
-  content_main.style.display = "flex";
+  console.log("num : ", num);
 
   if (num == 0) {
+    page(page_num);
+  } else {
+    content_main = content_list[num - 1];
+    content_main.style.display = "flex";
+  }
+
+  if (page_num == 0 && num == 1) {
     document.getElementById("left").style.display = "none";
   }
-  if (num != list_length - 1) {
-    document.getElementById("right").style.display = "block";
-  }
 }
-function next_btn() {
+
+// 다음
+function next_btn(page_num) {
+  document.getElementById("left").style.display = "block";
+
   content_main.style.display = "none";
   num++;
-  content_main = content_list[num];
-  content_main.style.display = "flex";
+  console.log("num : ", num);
 
-  if (num != 0) {
-    document.getElementById("left").style.display = "block";
+  if (num == list_length + 1) {
+    page(page_num);
+
+    num = 1;
+  } else {
+    content_main = content_list[num - 1];
+    content_main.style.display = "flex";
   }
-  if (num == list_length - 1) {
+}
+
+function page(page_num) {
+  if (page_num == 6) {
+    document.getElementById("go-home").style.display = "flex";
     document.getElementById("right").style.display = "none";
+  } else {
+    window.location.href = "mucha-section.html#sec" + page_num;
   }
 }
